510abd014263d7aaa7b236720be45d1824269452
Bryan Thomas <thomas.bryan.m@gmail.com>
2023-01-17 13:28:29 -0800

    Implement Routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Footer from './Components/NavigationBar/Footer/Footer';
 import NavigationBar from './Components/NavigationBar/NavigationBar';
+import Templates from './Components/Templates/Templates';
+import CreateEstimate from './Components/Estimates/CreateEstimate/CreateEstimate';
+import PastEstimates from './Components/Estimates/PastEstimates/PastEstimates';
+import NotFound from './Components/NotFound/NotFound';
 
 function App() {
   return (
@@ -9,6 +13,11 @@ function App() {
       <NavigationBar />
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<NotFound />} />
+          <Route path="/templates" element={<Templates />} />
+          <Route path="/create-estimate" element={<CreateEstimate />} />
+          <Route path="/past-estimates" element={<PastEstimates />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
       <Footer/>
