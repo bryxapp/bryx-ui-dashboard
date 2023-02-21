7e0dbd5a4f5f8d68ea42892280b3b1ad91942fda
Bryan Thomas <thomas.bryan.m@gmail.com>
2023-02-20 20:13:31 -0800

    Add Share button

--- src/Components/Estimates/ViewEstimate/ViewEstimate.tsx
+++ src/Components/Estimates/ViewEstimate/ViewEstimate.tsx
@@ -1,4 +1,4 @@
-import { Typography } from "@mui/material";
+import { Button, Typography } from "@mui/material";
 import { useState, useEffect } from "react";
 import { useLocation } from 'react-router-dom';
 import { getEstimate } from "../../../utils/estimates-api";
@@ -33,13 +33,26 @@ const ViewEstimate = () => {
         );
     }
 
+    const handleShareClick = () => {
+        const subject = `Estimate: ${estimate?.estimateName}`;
+        const body = `Hi,\n\nPlease find attached  the estimate for ${estimate?.estimateName}.\n\nBest regards\n`;
+        const mailtoLink = `mailto:?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}&attachment=${encodeURIComponent(estimate?.estimatePdfUrl || '')}`;
+        window.location.href = mailtoLink;
+    };
+
     return (
         <div>
             <Typography variant="h3" color="primary">
                 Name: {estimate?.estimateName}
             </Typography>
             <div style={{ height: 20 }}></div>
-            <iframe src={estimate?.estimatePdfUrl} width="100%" height="900" title="PDF"></iframe>
+            <div>
+                <Button variant="contained" color="primary" onClick={handleShareClick}>Share</Button>
+            </div>
+            <div style={{ height: 20 }}></div>
+            <div>
+                <iframe src={estimate?.estimatePdfUrl} width="100%" height="900" title="PDF"></iframe>
+            </div>
         </div>
     );
 }
