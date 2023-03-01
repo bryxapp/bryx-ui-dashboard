acc3779904216f4884ff045d7069fc77ec993c9a
Bryan Thomas <thomas.bryan.m@gmail.com>
2023-02-28 16:06:56 -0800

    Update View Estimate Buttons

--- src/Components/Estimates/ViewEstimate/ViewEstimate.tsx
+++ src/Components/Estimates/ViewEstimate/ViewEstimate.tsx
@@ -1,13 +1,21 @@
 import { useState, useEffect } from "react";
-import { Button, Link, Typography } from "@mui/material";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Link from "@mui/material/Link";
+import Tooltip from "@mui/material/Tooltip";
 import LinkIcon from "@mui/icons-material/Link"
-import ClipBoardIcon from "@mui/icons-material/CopyAll"
-import EmailIcon from "@mui/icons-material/Email"
+import ClipBoardIcon from "@mui/icons-material/ContentPaste"
+import ShareIcon from "@mui/icons-material/IosShare"
 import { useLocation } from 'react-router-dom';
 import { getEstimate } from "../../../utils/estimates-api";
 import { EstimateData } from "../../../utils/types/EstimateInterfaces";
 import Loading from "../../SharedComponents/Loading/Loading";
+import styled from "@emotion/styled";
 
+
+const EstimateButton = styled(Button)`
+    margin-right: 10px;
+`;
 
 const ViewEstimate = () => {
     const location = useLocation();
@@ -45,8 +53,7 @@ const ViewEstimate = () => {
 
     const handleLinkClick = () => {
         //Copy to clipboard
-        const link = estimate?.estimatePdfUrl;
-        console.log(link)
+        navigator.clipboard.writeText(estimate?.estimatePdfUrl || '');
     };
 
     return (
@@ -56,10 +63,23 @@ const ViewEstimate = () => {
             </Typography>
             <div style={{ height: 20 }}></div>
             <div>
-                <Button variant="contained" color="primary" onClick={handleShareClick}><EmailIcon /></Button>
-                <Link href={estimate?.estimatePdfUrl} target="_blank"><Button variant="contained" color="primary"> <LinkIcon /></Button>
-                </Link>
-                <Button variant="contained" color="primary" onClick={handleLinkClick}> <ClipBoardIcon /> </Button>
+                <Tooltip title="Share via email">
+                    <EstimateButton variant="contained" color="primary" onClick={handleShareClick}>
+                        <ShareIcon />
+                    </EstimateButton>
+                </Tooltip>
+                <Tooltip title="Open in new tab">
+                    <Link href={estimate?.estimatePdfUrl} target="_blank">
+                        <EstimateButton variant="contained" color="primary">
+                            <LinkIcon />
+                        </EstimateButton>
+                    </Link>
+                </Tooltip>
+                <Tooltip title="Copy link to clipboard">
+                    <EstimateButton variant="contained" color="primary" onClick={handleLinkClick}>
+                        <ClipBoardIcon />
+                    </EstimateButton>
+                </Tooltip>
             </div>
             <div style={{ height: 20 }}></div>
             <div>
