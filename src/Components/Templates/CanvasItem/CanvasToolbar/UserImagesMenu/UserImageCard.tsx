a87a3afe650467484f168576d73a1b4bdbf6f798
Bryan Thomas <thomas.bryan.m@gmail.com>
2023-04-08 23:23:44 -0700

    Add Image Upload Feature

---
+++ src/Components/Templates/CanvasItem/CanvasToolbar/UserImagesMenu/UserImageCard.tsx
@@ -0,0 +1,85 @@
+import * as React from 'react';
+import { useCallback } from 'react';
+import Card from '@mui/material/Card';
+import CardActionArea from '@mui/material/CardActionArea';
+import CardMedia from '@mui/material/CardMedia';
+import DeleteIcon from '@mui/icons-material/Clear';
+import { CanvasDesignData, ImageObj } from '../../../../../utils/types/CanvasInterfaces';
+import { createImageObj } from '../../../../../utils/types/ShapesFactory';
+import { deleteImage } from '../../../../../utils/user-images-api';
+import Box from '@mui/material/Box';
+
+interface UserImageProps {
+    setCanvasDesign: React.Dispatch<React.SetStateAction<CanvasDesignData>>;
+    imageData: ImageData;
+    userImages: Array<ImageData>;
+    setUserImages: React.Dispatch<React.SetStateAction<Array<ImageData>>>;
+    setAnchorEl: React.Dispatch<React.SetStateAction<HTMLElement | null>>;
+
+}
+
+type ImageData = {
+    url: string;
+    width: number;
+    height: number;
+    imageDbId: string;
+};
+
+
+export default function UserImageCard({ setCanvasDesign, imageData, setAnchorEl, userImages, setUserImages }: UserImageProps) {
+
+    const handleImageClick = useCallback((imageData: ImageData) => {
+        //Create a canvas image object
+        const newCanvasImage: ImageObj = createImageObj(imageData.url, imageData.width / 15, imageData.height / 15);
+        //Add it to the canvas
+        setCanvasDesign((prevCanvasDesign: CanvasDesignData) => ({
+            ...prevCanvasDesign,
+            Shapes: [...prevCanvasDesign.Shapes, newCanvasImage]
+        }));
+
+        setAnchorEl(null);
+    }, [setCanvasDesign, setAnchorEl]);
+
+    const handleImageDelete = useCallback(
+        async (selectedToDeleteImageUrl: string) => {
+            // Delete the image using the API utility
+            let imageDBIdToDelete = userImages.find((image) => image.url === selectedToDeleteImageUrl)?.imageDbId;
+            if (!imageDBIdToDelete) return;
+            await deleteImage(imageDBIdToDelete);
+            // Remove the deleted image from the local state
+            setUserImages((prevImages) =>
+                prevImages.filter((imageData) => imageData.url !== selectedToDeleteImageUrl)
+            );
+        },
+        [setUserImages, userImages]
+    );
+
+
+    return (
+        <Card>
+            <CardActionArea onClick={() => handleImageClick(imageData)}>
+                <Box position="relative">
+                    <CardMedia component="img" src={imageData.url} alt="Image thumbnail" />
+                    <div
+                        style={{
+                            cursor: 'pointer',
+                            position: 'absolute',
+                            top: 0,
+                            right: 0,
+                            backgroundColor: 'rgba(128, 128, 128, 0.7)',
+                            padding: 4,
+                            borderRadius: '50%',
+                        }}
+                        onClick={(event) => {
+                            event.stopPropagation(); // Prevent click event from propagating to the CardActionArea
+                            handleImageDelete(imageData.url);
+                        }}
+                    >
+                        <DeleteIcon />
+                    </div>
+                </Box>
+            </CardActionArea>
+        </Card>
+    )
+
+}
--- src/Components/Templates/CanvasItem/CanvasToolbar/UserImagesMenu/UserImages.tsx
+++ src/Components/Templates/CanvasItem/CanvasToolbar/UserImagesMenu/UserImages.tsx
@@ -1,63 +1,59 @@
 import * as React from 'react';
 import { useState, useEffect, useCallback } from 'react';
-import { CanvasDesignData, ImageObj } from '../../../../../utils/types/CanvasInterfaces';
-import { createImageObj } from '../../../../../utils/types/ShapesFactory';
-import Grid from '@mui/material/Grid';
-import Card from '@mui/material/Card';
-import CardMedia from '@mui/material/CardMedia';
-import CardActionArea from '@mui/material/CardActionArea';
-import { getUserImages, uploadImage } from '../../../../../utils/user-images-api';
-import { useAuth0 } from '@auth0/auth0-react';
-import Loading from '../../../../SharedComponents/Loading/Loading';
 import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
 import IconButton from '@mui/material/IconButton';
 import Menu from '@mui/material/Menu';
-import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 import Tooltip from '@mui/material/Tooltip';
+import Typography from '@mui/material/Typography';
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import { useAuth0 } from '@auth0/auth0-react';
+import { CanvasDesignData } from '../../../../../utils/types/CanvasInterfaces';
+import { getUserImages, uploadImage } from '../../../../../utils/user-images-api';
+import Loading from '../../../../SharedComponents/Loading/Loading';
+import UserImageCard from './UserImageCard';
 
 interface UserImagesMenuProps {
     isLoading: boolean;
     canvasDesign: CanvasDesignData;
-    setCanvasDesign: React.SetStateAction<any>;
+    setCanvasDesign: React.Dispatch<React.SetStateAction<CanvasDesignData>>;
 }
 
-export default function UserImagesMenu({ isLoading, canvasDesign, setCanvasDesign }: UserImagesMenuProps) {
+
+type ImageApiResponse = {
+    imageBlobUrl: string;
+    id: string;
+};
+
+export default function UserImagesMenu({ isLoading, setCanvasDesign }: UserImagesMenuProps) {
     const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-    const [images, setImages] = useState<Array<{ url: string; width: number; height: number; }>>([]);
-    const [loading, setLoading] = useState(true);
+
+    const [userImages, setUserImages] = useState<Array<{ url: string; width: number; height: number; imageDbId: string }>>([]);
+    const [fetchingUserImages, setFetchingUserImages] = useState(true);
     const { user } = useAuth0();
     const userId = user?.email || '';
-
-    const handleImageClick = useCallback((imageData: { url: string; width: number; height: number; }) => {
-        const newImage: ImageObj = createImageObj(imageData.url, imageData.width / 15, imageData.height / 15);
-
-        setCanvasDesign((prevCanvasDesign: CanvasDesignData) => ({
-            ...prevCanvasDesign,
-            Shapes: [...prevCanvasDesign.Shapes, newImage]
-        }));
-        setAnchorEl(null);
-    }, [setCanvasDesign, setAnchorEl]);
 
     const handleImageUpload = useCallback(async (event: React.ChangeEvent<HTMLInputElement>) => {
         const file = event.target.files?.[0];
         if (!file) return;
+        setFetchingUserImages(true);
         await uploadImage(userId, file);
         await getUserImages(userId).then(async (response) => {
             const results = response.data;
-            const imagePromises = results.map(async (image: any) => {
+            const imagePromises = results.map(async (image: ImageApiResponse) => {
                 const { width, height } = await getImageDimensions(image.imageBlobUrl);
-                return { url: image.imageBlobUrl, width, height };
+                return { url: image.imageBlobUrl, width, height, imageDbId: image.id };
             });
-
             const imageData = await Promise.all(imagePromises);
-            setImages(imageData);
+            setUserImages(imageData);
+            setFetchingUserImages(false);
         });
     }, [userId]);
 
+
     useEffect(() => {
         if (!userId) {
-            setLoading(false);
+            setFetchingUserImages(false);
             return;
         }
         const fetchUserImages = async () => {
@@ -66,12 +62,12 @@ export default function UserImagesMenu({ isLoading, canvasDesign, setCanvasDesig
 
             const imagePromises = results.map(async (image: any) => {
                 const { width, height } = await getImageDimensions(image.imageBlobUrl);
-                return { url: image.imageBlobUrl, width, height };
+                return { url: image.imageBlobUrl, width, height, imageDbId: image.id };
             });
 
             const imageData = await Promise.all(imagePromises);
-            setImages(imageData);
-            setLoading(false);
+            setUserImages(imageData);
+            setFetchingUserImages(false);
         };
 
         fetchUserImages();
@@ -85,8 +81,6 @@ export default function UserImagesMenu({ isLoading, canvasDesign, setCanvasDesig
             img.src = url;
         });
     };
-
-    if (loading) return <Loading />
 
     return (
         <>
@@ -121,17 +115,23 @@ export default function UserImagesMenu({ isLoading, canvasDesign, setCanvasDesig
                     <Typography variant="body2">
                         Use your own images
                     </Typography>
-                    <Grid container spacing={2} sx={{ p: 2 }}>
-                        {images.map((imageData) => (
-                            <Grid item xs={6} sm={4} md={3} key={imageData.url}>
-                                <Card>
-                                    <CardActionArea onClick={() => handleImageClick(imageData)}>
-                                        <CardMedia component="img" src={imageData.url} alt="Image thumbnail" />
-                                    </CardActionArea>
-                                </Card>
-                            </Grid>
-                        ))}
-                    </Grid>
+                    {fetchingUserImages ? (
+                        <Loading />
+                    ) : (
+                        <Grid container spacing={2} sx={{ p: 2 }}>
+                            {userImages.map((imageData) => (
+                                <Grid item xs={6} sm={4} md={3} key={imageData.url}>
+                                    <UserImageCard
+                                        setCanvasDesign={setCanvasDesign}
+                                        imageData={imageData}
+                                        userImages={userImages}
+                                        setUserImages={setUserImages}
+                                        setAnchorEl={setAnchorEl}
+                                    />
+                                </Grid>
+                            ))}
+                        </Grid>
+                    )}
                     <Button variant="contained" component="label">
                         Upload File
                         <input
