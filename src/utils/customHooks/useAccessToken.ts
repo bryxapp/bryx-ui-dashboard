d0d3659fde6692ba9eb392815ca630211f6b8eb1
Bryan Thomas <thomas.bryan.m@gmail.com>
2023-08-11 21:53:51 -0700

    Update custom hook

--- src/utils/customHooks/useAccessToken.ts
+++ src/utils/customHooks/useAccessToken.ts
@@ -1,10 +1,11 @@
+import { useCallback } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 
 export const useAccessToken = () => {
   const { user, getAccessTokenSilently } = useAuth0();
   const userId = user?.sub || "";
 
-  const getAccessToken = async () => {
+  const getAccessToken = useCallback(async () => {
     if (!userId) return null;
     return getAccessTokenSilently({
       authorizationParams: {
@@ -12,7 +13,7 @@ export const useAccessToken = () => {
         scope: "read:current_user update:current_user_metadata",
       }
     });
-  };
+  }, [userId, getAccessTokenSilently]);
 
   return { userId, getAccessToken };
 };
