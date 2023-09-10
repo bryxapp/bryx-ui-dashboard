ce7ccea03d906cca7748ec2d03c4ab048adc7bfb
Bryan Thomas <thomas.bryan.m@gmail.com>
2023-09-10 02:29:26 -0700

    Update login page

--- src/Components/NotLoggedIn/NotLoggedIn.tsx
+++ src/Components/NotLoggedIn/NotLoggedIn.tsx
@@ -1,17 +1,30 @@
-import { Typography, Button } from "@mui/material";
+import { Typography, Button, Box } from "@mui/material";
 import { useAuth0 } from "@auth0/auth0-react";
-
 
 const NotLoggedIn = () => {
     const { loginWithRedirect } = useAuth0();
 
     return (
-        <Typography variant="h4" component="h4" align="center">
-            Please log in to continue
-            <br />
-            <br />
-            <Button variant="contained" color="primary" sx={{ "fontSize": '1.2rem' }} onClick={() => loginWithRedirect()}>Log In</Button>
-        </Typography>
+        <Box
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+            justifyContent="center"
+            height="100vh" // This will center content vertically on the viewport
+        >
+            <Typography variant="h4" component="h4" marginBottom={3}>
+                Please log in to continue
+            </Typography>
+
+            <Button
+                variant="contained"
+                color="primary"
+                sx={{ fontSize: '1.2rem', marginRight: '1rem' }}
+                onClick={() => loginWithRedirect()}
+            >
+                Log In/Sign Up
+            </Button>
+        </Box >
     );
 };
 
