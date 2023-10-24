6a159521b2477519b8730c45360441ab92797457
Bryan Thomas <thomas.bryan.m@gmail.com>
2023-10-24 14:50:20 -0700

    Update logout behavior

--- src/Components/Navigation/TopNavBar/Logo.tsx
+++ src/Components/Navigation/TopNavBar/Logo.tsx
@@ -1,22 +1,30 @@
 import { Typography, useTheme } from '@mui/material';
+import { Link } from 'react-router-dom'; // Import Link from react-router-dom
 
 const Logo = () => {
     const theme = useTheme();
+    
+    const handleLogoClick = () => {
+        window.location.href = 'https://www.bryxbids.com/'; // Navigate to Home Site
+    };
+    
     return (
-        <Typography 
-            variant="h1" 
-            color={theme.palette.text.secondary} 
-            noWrap
-            sx={{
-                fontSize: '4rem',
-                fontWeight: 'bold',
-                '@media (max-width: 600px)': {
-                    fontSize: '2rem',
-                },
-            }}
-        >
-            BRYX bids
-        </Typography>
+        <Link to="/" onClick={handleLogoClick}> {/* Use Link from react-router-dom */}
+            <Typography 
+                variant="h1" 
+                color={theme.palette.text.secondary} 
+                noWrap
+                sx={{
+                    fontSize: '4rem',
+                    fontWeight: 'bold',
+                    '@media (max-width: 600px)': {
+                        fontSize: '2rem',
+                    },
+                }}
+            >
+                BRYX bids
+            </Typography>
+        </Link>
     );
 };
 
