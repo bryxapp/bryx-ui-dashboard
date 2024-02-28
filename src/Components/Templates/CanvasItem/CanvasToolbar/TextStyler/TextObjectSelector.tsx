23ba99940c97189a81d6071b087a28c5666ecff3
Bryan Thomas <thomas.bryan.m@gmail.com>
2024-02-27 21:18:37 -0800

    Add TextObject Selector

---
+++ src/Components/Templates/CanvasItem/CanvasToolbar/TextStyler/TextObjectSelector.tsx
@@ -0,0 +1,116 @@
+import React from 'react';
+import Typography from "@mui/material/Typography";
+import { CanvasDesignData, ShapeObj, TextFieldObj, TextInputObj, TextTableObj } from '../../../../../utils/types/CanvasInterfaces';
+import { findShape } from '../../../../../utils/canvas-util';
+import Select from '@mui/material/Select';
+import { MenuItem } from '@mui/material';
+
+
+interface TextObjectSelectorProps {
+    canvasDesign: CanvasDesignData;
+    setCanvasDesign: React.SetStateAction<any>;
+}
+
+const TextObjectSelector = ({ canvasDesign, setCanvasDesign }: TextObjectSelectorProps) => {
+
+    const convertTextObject = (shape: ShapeObj, type: string) => {
+        switch (type) {
+            case "TextInput":
+                {
+                    const TextFieldObj = shape as TextFieldObj;
+                    let { id, x, y, value, fontSize, fontFamily, fontStyle, textDecoration, align, rotation, isDragging, fill } = TextFieldObj;
+                    return {
+                        id,
+                        x,
+                        y,
+                        fontSize,
+                        fontFamily,
+                        fontStyle,
+                        textDecoration,
+                        align,
+                        type: "TextInput",
+                        displayName: value,
+                        rotation,
+                        isDragging,
+                        fill,
+                        format: "text"
+                    };
+                }
+            case "TextField":
+                {
+                    const TextInputObj = shape as TextInputObj;
+                    const { id, x, y, displayName, fontSize, fontFamily, fontStyle, textDecoration, align, rotation, isDragging, fill } = TextInputObj;
+                    return {
+                        id,
+                        x,
+                        y,
+                        fontSize,
+                        fontFamily,
+                        fontStyle,
+                        textDecoration,
+                        align,
+                        type: "TextField",
+                        value: displayName,
+                        rotation,
+                        isDragging,
+                        fill
+                    };
+                }
+            default:
+                return shape;
+        }
+    };
+    const handleTextObjectChange = (event: any) => {
+        let foundAndUpdated = false;
+
+        const updateShape = (shape: ShapeObj) => {
+            if (shape.id === canvasDesign.selectedId && !foundAndUpdated) {
+                foundAndUpdated = true;
+                return convertTextObject(shape, event.target.value);
+            }
+            return shape;
+        };
+
+        const updatedShapes = canvasDesign.Shapes.map((shape: ShapeObj) => {
+            if (shape.type === "TextTable") {
+                const textTable = shape as TextTableObj;
+                const updatedRows = textTable.rows.map(row =>
+                    row.map(cell => updateShape(cell))
+                );
+                return { ...shape, rows: updatedRows };
+            }
+            return updateShape(shape);
+        });
+
+        if (foundAndUpdated) {
+            setCanvasDesign({ ...canvasDesign, Shapes: updatedShapes });
+        }
+    };
+
+
+    const selectedTextObjectType = (findShape(canvasDesign, canvasDesign.selectedId))?.type ?? '';
+
+    return (
+        <>
+            <Typography variant="body1" >
+                Type
+            </Typography>
+            <Select
+                value={selectedTextObjectType || ''}
+                onChange={handleTextObjectChange}
+                variant="outlined"
+                style={{ marginBottom: '1rem', minWidth: '4.5rem', margin: 10 }}
+                size='small'
+            >
+                <MenuItem key={"TextInput"} value={"TextInput"}>
+                    Text Input
+                </MenuItem>
+                <MenuItem key={"TextField"} value={"TextField"}>
+                    Text Field
+                </MenuItem>
+            </Select>
+        </>
+    );
+};
+
+export default TextObjectSelector;
--- src/Components/Templates/CanvasItem/CanvasToolbar/TextStyler/TextStyler.tsx
+++ src/Components/Templates/CanvasItem/CanvasToolbar/TextStyler/TextStyler.tsx
@@ -7,7 +7,8 @@ import FontDecorationPicker from './FontDecorationPicker'
 import InputFormatPicker from './InputFormatPicker';
 import AlignmentPicker from './AlignmentPicker';
 import { AppBar } from '@mui/material';
-import { findShape, isTextObject } from '../../../../../utils/canvas-util';
+import { findShape, isNested, isTextObject } from '../../../../../utils/canvas-util';
+import TextObjectSelector from './TextObjectSelector';
 
 interface TextStylerProps {
     canvasDesign: CanvasDesignData;
@@ -16,6 +17,7 @@ interface TextStylerProps {
 
 function TextStyler({ canvasDesign, setCanvasDesign }: TextStylerProps) {
     if (!isTextObject(findShape(canvasDesign, canvasDesign.selectedId))) return null;
+    const isNestedShape = isNested(canvasDesign, canvasDesign.selectedId);
 
     return (
         <AppBar position="static" color='secondary' style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap', alignItems: 'center', paddingLeft: "1rem" }}>
@@ -26,6 +28,7 @@ function TextStyler({ canvasDesign, setCanvasDesign }: TextStylerProps) {
             <FontStylePicker canvasDesign={canvasDesign} setCanvasDesign={setCanvasDesign} />
             <FontDecorationPicker canvasDesign={canvasDesign} setCanvasDesign={setCanvasDesign} />
             <AlignmentPicker canvasDesign={canvasDesign} setCanvasDesign={setCanvasDesign} />
+            {isNestedShape && <TextObjectSelector canvasDesign={canvasDesign} setCanvasDesign={setCanvasDesign} />}
         </AppBar>
     );
 }
